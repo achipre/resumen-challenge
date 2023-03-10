461ee1d9564ebb9b43a501074b3c66c6056f37a7
Alex Chipre <alechipre@gmail.com>
2023-03-10 10:53:49 -0500

    Finish Resumen CV

--- script/darkmode.js
+++ script/darkmode.js
@@ -1,6 +1,7 @@
 const htmldark = document.querySelector('html');
 const btnDark = document.querySelector('.modedark');
-const btnMoon = document.querySelectorAll('.btndark');
+const btnMoon = document.querySelector('.moon');
+const btnSun = document.querySelector('.sun');
 const backSoft = document.querySelectorAll('.backsoft');
 const backHard = document.querySelectorAll('.backhard');
 
@@ -17,10 +18,13 @@ const btnHigh = document.querySelectorAll('.experience__botao--demo');
 const input = document.querySelectorAll('.formcontato__input');
 const inputtext = document.querySelector('.formcontato__textarea');
 
+load();
+
 btnDark.addEventListener('click', lightAndDark)
 function lightAndDark() {
   htmldark.classList.toggle('background__dark--soft');
-  btnMoon.forEach(e => e.classList.toggle('disable')) ;
+  btnMoon.classList.toggle('disable');
+  btnSun.classList.toggle('disable') ;
   backSoft.forEach(e => e.classList.toggle('background__dark--soft')) ;
   backHard.forEach(e => e.classList.toggle('background__dark--hard')) ;
   icon.forEach(e => e.classList.toggle('icon__mode-dark',)) ;
@@ -36,4 +40,51 @@ function lightAndDark() {
   inputtext.classList.toggle('background__dark--soft');
   inputtext.classList.toggle('fuente__dark');
 
+  store(htmldark.classList.contains('background__dark--soft'));
+  store(btnMoon.classList.contains('disable'));
+  store(btnSun.classList.contains('disable')) ;
+  store(backSoft.forEach(e => e.classList.contains('background__dark--soft')) );
+  store(backHard.forEach(e => e.classList.contains('background__dark--hard')) );
+  store(icon.forEach(e => e.classList.contains('icon__mode-dark')) );
+  store(iconSyH.forEach(e => e.classList.contains('icon__mode-dark'))) ;
+  store(firma.classList.contains('firma__mode-dark'));
+  store(fuente.forEach(e => e.classList.contains('fuente__dark')));
+  store(card.forEach(e => e.classList.contains('backround__card')));
+  store(imgAcademico.forEach(e => e.classList.contains('img-invert')));
+  store(btnLow.forEach(e => e.classList.contains('btn__low')));
+  store(btnHigh.forEach(e => e.classList.contains('btn__high')));
+  store(input.forEach(e => e.classList.contains('background__dark--soft')));
+  store(input.forEach(e => e.classList.contains('fuente__dark')));
+  store(inputtext.classList.contains('background__dark--soft'));
+  store(inputtext.classList.contains('background__dark--soft'));
+  store(inputtext.classList.contains('fuente__dark'));
+
+}
+function load() {
+  const darkmode = localStorage.getItem('darkmode')
+  if (!darkmode) {
+    store('false')
+  } else if (darkmode == 'true') {
+    htmldark.classList.add('background__dark--soft');
+    btnMoon.classList.add('disable') ;
+    btnSun.classList.remove('disable') ;
+    backSoft.forEach(e => e.classList.add('background__dark--soft')) ;
+    backHard.forEach(e => e.classList.add('background__dark--hard')) ;
+    icon.forEach(e => e.classList.add('icon__mode-dark',)) ;
+    iconSyH.forEach(e => e.classList.add('icon__mode-dark',)) ;
+    firma.classList.add('firma__mode-dark');
+    fuente.forEach(e => e.classList.add('fuente__dark'));
+    card.forEach(e => e.classList.add('backround__card'));
+    imgAcademico.forEach(e => e.classList.add('img-invert'));
+    btnLow.forEach(e => e.classList.add('btn__low'));
+    btnHigh.forEach(e => e.classList.add('btn__high'));
+    input.forEach(e => e.classList.add('background__dark--soft'));
+    input.forEach(e => e.classList.add('fuente__dark'));
+    inputtext.classList.add('background__dark--soft');
+    inputtext.classList.add('fuente__dark');
+  }
+}
+
+function store(value) {
+  localStorage.setItem('darkmode', value)
 }
